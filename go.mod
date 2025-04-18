496321290d6ccaa9f60fd9419d337c0410e2ece1
Robson Fischer <fischerrobson@gmail.com>
2025-04-17 21:59:25 -0300

    major fix on parsing form data

--- go.mod
+++ go.mod
@@ -1,16 +1,16 @@
 module github.com/FischerRobson/help.me-upload
 
-go 1.22
+go 1.21
 
 require (
-	github.com/aws/aws-sdk-go v1.55.5
+	github.com/aws/aws-sdk-go v1.55.6
 	github.com/go-chi/chi v1.5.5
-	github.com/go-chi/chi/v5 v5.1.0
+	github.com/go-chi/chi/v5 v5.2.1
 	github.com/go-chi/cors v1.2.1
 )
 
 require (
-	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/golang-jwt/jwt/v5 v5.2.2
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
 	github.com/joho/godotenv v1.5.1
 	github.com/streadway/amqp v1.1.0
--- internal/api/handlersV2.go
+++ internal/api/handlersV2.go
@@ -20,16 +20,16 @@ type UploadResponse struct {
 }
 
 func (h apiHandler) uploadFileWithRabbitMQ(w http.ResponseWriter, r *http.Request) {
-	uploadId := r.FormValue("uploadId")
-	if uploadId == "" {
-		http.Error(w, "Missing uploadId", http.StatusBadRequest)
-		return
-	}
-
 	err := r.ParseMultipartForm(MAX_UPLOAD_SIZE)
 	if err != nil {
 		http.Error(w, "Something went wrong", http.StatusInternalServerError)
 		slog.Error("Failed to parse multipart form", err)
+		return
+	}
+
+	uploadId := r.FormValue("uploadId")
+	if uploadId == "" {
+		http.Error(w, "Missing uploadId", http.StatusBadRequest)
 		return
 	}
 
@@ -40,6 +40,8 @@ func (h apiHandler) uploadFileWithRabbitMQ(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
+	slog.Info("Parsed Form:", "files", len(files), "uploadId", uploadId)
+
 	resp := response{}
 
 	w.WriteHeader(http.StatusOK)
@@ -47,64 +49,64 @@ func (h apiHandler) uploadFileWithRabbitMQ(w http.ResponseWriter, r *http.Reques
 
 	uploadToS3 := os.Getenv("ENABLE_AWS_UPLOAD") == "true"
 
-	go func() {
-		for _, fileHeader := range files {
-			file, err := fileHeader.Open()
+	// go func() {
+	for _, fileHeader := range files {
+		file, err := fileHeader.Open()
+		if err != nil {
+			http.Error(w, "Something went wrong", http.StatusInternalServerError)
+			slog.Error(fmt.Sprintf("Failed to open file %s", fileHeader.Filename))
+			return
+		}
+		defer file.Close()
+
+		if uploadToS3 {
+			fileURL, err := awsProvider.UploadToS3(h.s3Client, w, h.bucket, fileHeader)
+			if err != nil {
+				continue
+			}
+			fmt.Print(fileURL)
+
+			resp.Urls = append(resp.Urls, fileURL)
+		} else {
+
+			dst, err := os.Create(filepath.Join("uploads", fileHeader.Filename))
 			if err != nil {
 				http.Error(w, "Something went wrong", http.StatusInternalServerError)
-				slog.Error(fmt.Sprintf("Failed to open file %s", fileHeader.Filename))
+				slog.Error(fmt.Sprintf("Failed to open dst %s", dst.Name()))
 				return
 			}
-			defer file.Close()
+			defer dst.Close()
 
-			if uploadToS3 {
-				fileURL, err := awsProvider.UploadToS3(h.s3Client, w, h.bucket, fileHeader)
-				if err != nil {
-					continue
-				}
-				fmt.Print(fileURL)
-
-				resp.Urls = append(resp.Urls, fileURL)
-			} else {
-
-				dst, err := os.Create(filepath.Join("uploads", fileHeader.Filename))
-				if err != nil {
-					http.Error(w, "Something went wrong", http.StatusInternalServerError)
-					slog.Error(fmt.Sprintf("Failed to open dst %s", dst.Name()))
-					return
-				}
-				defer dst.Close()
-
-				if _, err := io.Copy(dst, file); err != nil {
-					http.Error(w, "Something went wrong", http.StatusInternalServerError)
-					slog.Error(fmt.Sprintf("Failed to save file %s", fileHeader.Filename))
-					return
-				}
+			if _, err := io.Copy(dst, file); err != nil {
+				http.Error(w, "Something went wrong", http.StatusInternalServerError)
+				slog.Error(fmt.Sprintf("Failed to save file %s", fileHeader.Filename))
+				return
 			}
 		}
+	}
 
-		queueName := os.Getenv("UPLOAD_FILE_QUEUE")
-		fmt.Print("Getting queue name")
-		if queueName == "" {
-			log.Fatal("UPLOAD_FILE_QUEUE environment variable is not set. Exiting application.")
-		}
+	queueName := os.Getenv("UPLOAD_FILE_QUEUE")
+	fmt.Print("Getting queue name")
+	if queueName == "" {
+		log.Fatal("UPLOAD_FILE_QUEUE environment variable is not set. Exiting application.")
+	}
 
-		message := UploadResponse{
-			UploadID: uploadId,
-			FileURLs: resp.Urls,
-		}
+	message := UploadResponse{
+		UploadID: uploadId,
+		FileURLs: resp.Urls,
+	}
 
-		messageBody, err := json.Marshal(message)
-		if err != nil {
-			fmt.Errorf("Failed to marshal message: %v", err)
-			return
-		}
+	messageBody, err := json.Marshal(message)
+	if err != nil {
+		fmt.Errorf("Failed to marshal message: %v", err)
+		return
+	}
 
-		err = h.rabbitMQ.PublishToQueue("fileUploadQueue", messageBody)
-		if err != nil {
-			log.Printf("Failed to publish message to RabbitMQ: %v", err)
-			http.Error(w, "Failed to upload file", http.StatusInternalServerError)
-			return
-		}
-	}()
+	err = h.rabbitMQ.PublishToQueue("fileUploadQueue", messageBody)
+	if err != nil {
+		log.Printf("Failed to publish message to RabbitMQ: %v", err)
+		http.Error(w, "Failed to upload file", http.StatusInternalServerError)
+		return
+	}
+	// }()
 }
